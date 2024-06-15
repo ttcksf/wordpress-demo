059c5d7075e71323f87b662b66b46464e464c43a
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-16 03:18:36 +0900

    enqueue-scripts

--- demo/functions.php
+++ demo/functions.php
@@ -1,20 +1,24 @@
 <?php
-  // echo "<pre>";
-  // print_r(get_template_directory_uri());
-  // print_r(get_stylesheet_uri());
-  // CSSを新しく追加してリロードするたびに最新のタイムスタンプになる
-  // 自動で出力されるlinkタグのstyle.cssの後にバージョン名として入る予定
-  // print_r(filemtime(get_template_directory() . "/style.css"));
-  // wp_die();
 
 
   function my_scripts(){
-    // 他にもCSSを読み込んでstyle.cssに依存関係を作るためにはこちら（依存関係に指定したものが先に読み込まれる仕組み）
-    wp_enqueue_style("sub-css", get_template_directory_uri() . "sub.css", ["stylesheet"]);
-    // style.cssだけならこちらのみでOK（依存関係がなければ空の配列）
-    // filemtimeはタイムスタンプを表示する関数（キャッシュを回避して最新バージョンを読み込める）
-    // メディアタイプは指定がなければallを選択しておく
-    wp_enqueue_style("stylesheet", get_stylesheet_uri(), [], filemtime(get_template_directory() . "/style.css"),"all");
+    // wp_enqueue_style("stylesheet", get_stylesheet_uri(), [], filemtime(get_template_directory() . "/style.css"),"all");
+    wp_register_style("stylesheet", get_stylesheet_uri(), [], filemtime(get_template_directory() . "/style.css"),"all");
+
+    // 第四引数はtrueにするとbodyタグの閉じタグで読み込まれて、falseにするとheadタグの閉じタグで読み込まれる
+    // wp_enqueue_script("script", get_template_directory_uri() . "/assets/script.js", [], filemtime(get_template_directory() . "/assets/script.js"), true);
+    wp_register_script("script", get_template_directory_uri() . "/assets/script.js", [], filemtime(get_template_directory() . "/assets/script.js"), true);
+
+    wp_enqueue_style("stylesheet");
+    wp_enqueue_script("script");
+
+    // 複数ファイルをページごとに出力し分けたい時に短くかけるので、一度wp_registerで登録して、ハンドルネームのみでwp_enqueueを呼び出す形が効率的
+    // if(is_archive()){
+    //   wp_enqueue_style("stylesheet");
+    // }elseif(is_page()){
+    //   wp_enqueue_style("sub");
+    // }
+    // 最後にscreenshotを登録して終わる
   }
   add_action("wp_enqueue_scripts", "my_scripts");
 ?>
