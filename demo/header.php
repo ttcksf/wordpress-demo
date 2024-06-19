ba76d656f1777d785c95f39711983da6982133e1
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-20 03:17:39 +0900

    get_template_part2

--- demo/header.php
+++ demo/header.php
@@ -6,5 +6,14 @@
   <title>Document</title>
   <?php wp_head();?>
 </head>
-<body <?php body_class("test-class");?>>
-  <header>ヘッダー</header>
+<body>
+<!-- https://getbootstrap.jp/docs/5.3/components/navbar/ -->
+  <div id="page" class="site">
+    <header id="masthead" class="site-header" role="banner">
+      <!-- ディレクトリまでのパスは自動出力される。ファイル名の拡張子はつけない -->
+      <?php get_template_part("templates/header/nav");?>
+    </header>
+    <div id="content" class="site-content">
+
+    </div>
+  </div>
