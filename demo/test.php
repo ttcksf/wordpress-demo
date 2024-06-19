3e21726171981da15a78e08a7b690b31ef11d0be
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-20 04:05:46 +0900

    singleton

--- demo/test.php
+++ demo/test.php
@@ -1,17 +1,27 @@
 <?php
- trait Say_World{
-  public function say_hello(){
-    echo "Hello";
+  trait Singleton{
+    public static function get_instance(){
+      static $instance = [];
+      // 呼び出されたクラスの名前を返す
+      $called_class = get_called_class();
+      // 一度もインスタンス化されてない時
+      if(!isset($instance[$called_class])){
+        echo "Hello";
+        // $instance["User"] = new User;が実行される
+        $instance[$called_class] = new $called_class;
+      }
+      return $instance[$called_class];
+    }
   }
- }
-//  通常のクラスのようにインスタンス化するものではない
-//  new Say_World()
-
-  class Base{
+  class User{
     // traitを使う合図
-    use Say_World;
+    use Singleton;
+    public function __construct()
+    {
+      echo "User";
+    }
   }
-  // traitを使うと複数クラスある時に継承の連鎖になりづらくなる
-  $base = new Base();
-  $base->say_hello();
+  // 自由に複製できるのがクラスの便利な点だが余計なインスタンス化を防ぎたい時
+  $user1 = User::get_instance();
+  $user2 = User::get_instance();
 ?>
