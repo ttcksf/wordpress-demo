0a4b0cb29d4c9b1ae6a789e1284ef603f3b7fe79
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-21 01:06:22 +0900

    header-menu3

--- demo/templates/header/nav.php
+++ demo/templates/header/nav.php
@@ -1,13 +1,7 @@
 <?php
-  // いきなり表示するのではなく一旦メニュー情報のみを取得する
   $locations = get_nav_menu_locations();
-  // var_dump($locations);
   $menu_id = $locations["header_menu"];
   $header_menus = wp_get_nav_menu_items($menu_id);
-  // echo "<pre>";
-  // print_r($header_menus);
-  // wp_die();
-  
 ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
@@ -20,28 +14,15 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+      <?php if(!empty($header_menus) && is_array($header_menus)):?>
+        <ul class="navbar-nav mr-auto">
+          <?php foreach($header_menus as $header_menu):?>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo esc_url($header_menu->url)?>"><?php echo esc_html($header_menu->title);?></a>
+            </li>
+          <?php endforeach;?>
+        </ul>
+      <?php endif;?>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
