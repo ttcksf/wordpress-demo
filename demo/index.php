4605f19696c9cff55610682ac3018915002d6317
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-25 03:54:22 +0900

    page-title

--- demo/index.php
+++ demo/index.php
@@ -3,15 +3,20 @@
     <main id="main" class="site-main mt-5" role="main">
       <?php if(have_posts()):?>
         <div class="container">
-          <?php while(have_posts()):
-                  the_post();
-          ?>
-            <?php the_title();?>
-            <!-- 抜粋文 -->
-            <?php the_excerpt();?>
-            <!-- 画像なども含めた全文 -->
-            <?php the_content();?>
-          <?php endwhile;?>
+          <!-- 固定ページで/topと/blogを作成して、表示設定でトップページを/topに投稿ページを/blogにしておく -->
+           <!-- ヘッダーメニューに/blogを追加しておく -->
+          <?php if(is_home() && !is_front_page()): ?>
+            <header class="mb-5">
+              <h1><?php single_post_title();?></h1>
+            </header>
+            <?php while(have_posts()):
+                    the_post();
+            ?>
+              <?php the_title();?>
+              <?php the_excerpt();?>
+              <?php the_content();?>
+            <?php endwhile;?>
+          <?php endif;?>
         </div>
       <?php endif;?>
     </main>
