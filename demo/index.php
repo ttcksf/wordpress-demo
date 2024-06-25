d24f0485a00f227bd21a268295ad72c4e3a57254
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-25 15:44:30 +0900

    grid-post

--- demo/index.php
+++ demo/index.php
@@ -3,19 +3,21 @@
     <main id="main" class="site-main mt-5" role="main">
       <?php if(have_posts()):?>
         <div class="container">
-          <!-- 固定ページで/topと/blogを作成して、表示設定でトップページを/topに投稿ページを/blogにしておく -->
-           <!-- ヘッダーメニューに/blogを追加しておく -->
+        <!-- https://getbootstrap.jp/docs/5.3/layout/grid/ -->
           <?php if(is_home() && !is_front_page()): ?>
             <header class="mb-5">
               <h1><?php single_post_title();?></h1>
             </header>
-            <?php while(have_posts()):
-                    the_post();
-            ?>
-              <?php the_title();?>
-              <?php the_excerpt();?>
-              <?php the_content();?>
-            <?php endwhile;?>
+            <div class="row">
+              <?php while(have_posts()):
+                      the_post();
+              ?>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                  <h3><?php the_title();?></h3>
+                  <div><?php the_excerpt();?></div>
+                </div>
+              <?php endwhile;?>
+            </div>
           <?php endif;?>
         </div>
       <?php endif;?>
