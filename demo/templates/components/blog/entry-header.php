12007685972b08ef4a338d55b36c7be32afd90c0
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-08 19:15:37 +0900

    no-image

--- demo/templates/components/blog/entry-header.php
+++ demo/templates/components/blog/entry-header.php
@@ -1,14 +1,20 @@
 <header class="entry-header">
   <?php
-    // サムネイル画像が設定されているか
     $the_post_id = get_the_ID();
     $has_post_thumbnail = get_the_post_thumbnail($the_post_id);
     if(has_post_thumbnail()):
   ?>
     <div class="entry-image mb-3">
       <a href="<?php echo esc_url(get_permalink());?>">
-        <?php the_post_thumbnail("thumbnail");?>
+        <!-- 第二引数がclass名 -->
+        <?php $attr = ['class' => 'img'];?>
+        <?php the_post_thumbnail("thumbnail",$attr);?>
       </a>
+    </div>
+  <?php else:?>
+    <!-- https://dummyimage.com/ -->
+    <div class="no-image">
+      <img src="https://dummyimage.com/150x150/000/fff">
     </div>
   <?php endif;?>
 </header>
