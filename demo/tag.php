8ace2e5706d01e2cd1ffab0828ea7db5bdc8ab6b
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-18 09:56:50 +0900

    the-archive-title

--- demo/tag.php
+++ demo/tag.php
@@ -3,9 +3,11 @@
     <main id="main" class="site-main mt-5" role="main">
       <?php if(have_posts()):?>
         <div class="container">
-          <?php if(is_home() && !is_front_page()): ?>
+          <!-- ページが変わったため条件分岐を変えておく -->
+          <?php if(is_tag() && !is_front_page()): ?>
             <header class="mb-5">
-              <h1><?php single_post_title();?></h1>
+              <!-- ページタイトルの関数も変わる -->
+              <h1><?php the_archive_title();?></h1>
               <p>カテゴリー一覧：<?php the_category(",");?></p>
               <?php the_tags("タグ一覧：");?>
 
