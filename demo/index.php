8eda38b56958d97c58b109007df5027834e01022
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-18 09:43:27 +0900

    cat-lists

--- demo/index.php
+++ demo/index.php
@@ -3,10 +3,14 @@
     <main id="main" class="site-main mt-5" role="main">
       <?php if(have_posts()):?>
         <div class="container">
-        <!-- https://getbootstrap.jp/docs/5.3/layout/grid/ -->
           <?php if(is_home() && !is_front_page()): ?>
             <header class="mb-5">
               <h1><?php single_post_title();?></h1>
+              <!-- 投稿に設定されたカテゴリーのみが取得される -->
+               <!-- デフォルトはulとliによる箇条書き -->
+              <!-- <?php the_category();?> -->
+               <!-- 引数に「,」を入れるとulとliがなくなる -->
+              <p>カテゴリー一覧：<?php the_category(",");?></p>
             </header>
             <div class="row">
               <?php while(have_posts()):
