bc858bef56dc7c3fb89a4d5f3418860dd894af3b
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-26 21:35:50 +0900

    404page

---
+++ demo/404.php
@@ -0,0 +1,10 @@
+<?php get_header();?>
+<!-- 404.phpがないと別のページが表示される仕組みになっている -->
+  <div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <div class="container">
+          <h1>お探しのページはありませんでした</h1>
+        </div>
+    </main>
+  </div>
+<?php get_footer();?>
