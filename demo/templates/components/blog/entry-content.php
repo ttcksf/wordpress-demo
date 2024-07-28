5d7ac9f820b587d49fea31b04547fe7c75508978
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-28 15:20:16 +0900

    wp-link-pages

--- demo/templates/components/blog/entry-content.php
+++ demo/templates/components/blog/entry-content.php
@@ -1,7 +1,10 @@
 <h2><?php the_title();?></h2>
 <?php if(is_single()):?>
   <?php the_content();?>
-  <?php wp_link_pages();?>
+  <?php wp_link_pages([
+    "pagelink" => '<button class="btn btn-info text-white">%</button>'
+  ]);?>
+
 <?php else:?>
   <?php the_excerpt();?>
 <?php endif;?>
