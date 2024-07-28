fea2f98c6e79956645759c10a1be0ef9613623ad
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-28 15:26:32 +0900

    page-numbers

--- demo/templates/components/blog/entry-content.php
+++ demo/templates/components/blog/entry-content.php
@@ -4,7 +4,10 @@
   <?php wp_link_pages([
     "pagelink" => '<button class="btn btn-info text-white">%</button>'
   ]);?>
-
+  <div class="mt-4 mb-4">
+    <?php previous_post_link();?>
+    <?php next_post_link();?>
+  </div>
 <?php else:?>
   <?php the_excerpt();?>
 <?php endif;?>
