41aee46f3a99477871d0fe6663ba4b03f1232ea8
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-08-01 03:07:59 +0900

    footer-advance

--- demo/templates/footer/nav.php
+++ demo/templates/footer/nav.php
@@ -1,13 +1,18 @@
+<?php
+  $locations = get_nav_menu_locations();
+  $menu_id = $locations["footer_menu"];
+  $footer_menus = wp_get_nav_menu_items($menu_id);
+?>
 <div class="row">
-  <ul class="nav justify-content-center py-5">
-    <li class="nav-item">
-      <a href="#" class="nav-link text-white">Home</a>
-    </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link text-white">page1</a>
-    </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link text-white">page2</a>
-    </li>
-  </ul>
+  <?php if(!empty($footer_menus) && is_array($footer_menus)):?>
+    <ul class="nav justify-content-center py-5">
+      <?php foreach($footer_menus as $footer_menu):?>
+        <li class="nav-item">
+          <a href="<?php echo esc_url($footer_menu->url); ?>" class="nav-link text-white">
+            <?php echo esc_html($footer_menu->title); ?>
+          </a>
+        </li>
+      <?php endforeach;?>
+    </ul>
+  <?php endif;?>
 </div>
